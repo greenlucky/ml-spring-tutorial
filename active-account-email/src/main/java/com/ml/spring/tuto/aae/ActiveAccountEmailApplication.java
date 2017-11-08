3c29d23b37d84857eb8ab493abf3ab6d19242dfd
GNT-GLOBAL\lam.nm <lam.nm@vn.gnt-global.com>
2017-11-08 16:34:08 +0700

    First push

---
+++ active-account-email/src/main/java/com/ml/spring/tuto/aae/ActiveAccountEmailApplication.java
@@ -0,0 +1,12 @@
+package com.ml.spring.tuto.aae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveAccountEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActiveAccountEmailApplication.class, args);
+	}
+}
